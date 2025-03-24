693965b34d5f157ef2572f5d09f8df8579694f16
chiaralle <chiaraallegrino@gmail.com>
2025-03-24 16:39:53 +0100

    add:header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,26 +6,45 @@ export default function Header() {
     const showSB = location.pathname.startsWith('/contacts')
 
     return (
-        <nav className=" navbar navbar-expand-md bg-body-tertiary  d-flex justify-content-between px-5">
 
-
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
             <Link className="navbar-brand" to='/'>
                 <img width="50px" src="/logo.png" className="img-fluid rounded" alt="logo" />
             </Link>
 
             {showSB && <SearchBar />}
 
-            <ul className="nav navbar-nav">
-                <li className="nav-item">
-                    <NavLink className="nav-link " to="/"> HOME </NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="/contacts">RUBRICA</NavLink>
-                </li>
+       
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuHamburger" 
+            aria-controls="menuHamburger" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-            </ul>
-        </nav>
+    
+            <div class="collapse navbar-collapse" id="menuHamburger">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <NavLink className="nav-link " to="/"> HOME </NavLink>
+                    </li>
+                    <li class="nav-item">
+                        <NavLink className="nav-link" to="/contacts">RUBRICA</NavLink>
+                    </li>
+                </ul>   
+      
+      
+                {/*<form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Cerca..." aria-label="Search"/>
+                    <button class="btn btn-outline-success" type="submit">Cerca</button>
+                </form>*/}
+            </div>
+        </div>
+    </nav>
+
+            
+
     )
-}
+
+ }
 
 
