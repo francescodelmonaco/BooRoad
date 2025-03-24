8010b60959dc55d6627fed3f8e344229ee34a020
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-03-24 16:46:53 +0100

    edit: search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,18 @@
-import { useTravelContext } from "../contexts/TravelContext"
-
+import { useTravelContext } from "../contexts/TravelContext";
 
 export default function SearchBar() {
-
-
     const { searchTerm, handleSearch, handleSubmit, refSearch } = useTravelContext();
     return (
-        <form className=" " onSubmit={handleSubmit} >
+        <form className="container d-flex mb-3" onSubmit={handleSubmit} >
             <input
                 ref={refSearch}
                 type="text"
-                placeholder="contatti in rubrica..."
+                placeholder="Cerca contatto"
                 value={searchTerm}
                 onChange={handleSearch}
+                className="form-control me-2 text-black bg-white"
             />
-        </form>
+            <button class="btn" type="submit">Cerca</button>
+        </form >
     )
-}
+};
