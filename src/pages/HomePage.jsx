ed42b4df622100a5af81d1ff2cd54e7f309aafdf
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-03-25 10:14:39 +0100

    edit card row-cols-lg-3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import Card from "../components/Card";
 export default function HomePage({ travelData }) {
     return (
         <div className="container mt-4">
-            <div className="row row-cols-1 row-cols-md-2 g-4 justify-content-center">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 justify-content-center">
                 {travelData.map(t => (
                     <div key={t.id} className="col d-flex justify-content-center">
                         <Link to={`/travels/${t.id}`} >
